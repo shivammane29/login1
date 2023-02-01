05c91cafcb85c6f62ab08a457b139fec406d61a1
shivammane29 <124154878+shivammane29@users.noreply.github.com>
2023-02-01 18:06:25 +0530

    Add files via upload

---
+++ LoginPage/src/main/webapp/LoginServlet.java
@@ -0,0 +1,35 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	
+       
+    
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String n = request.getParameter("t1");
+		String p = request.getParameter("t2");
+		
+		
+		if(n.equals("shivam") && p.equals("shivam123")) {
+			response.sendRedirect("success.jsp");
+		}
+		
+	}
+
+}
---
+++ LoginPage/src/main/webapp/input.jsp
@@ -0,0 +1,27 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Login Info</title>
+</head>
+<body>
+<form action="LoginServlet" method="post">
+<pre>
+
+			Enter User Name : <input type="text" name ="t1"> 
+			
+			Enter Password : <input type="password" name ="t2"> 
+			
+			<input type="submit" value="Login">
+   
+
+
+</pre>
+
+
+</form>
+
+</body>
+</html>
